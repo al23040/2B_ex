cce2cfc5456e388f849967c292de1798a0cd8459
al23040 <al23040@shibaura-it.ac.jp>
2025-12-31 17:47:15 +0900

    fix native_print

--- svm/native.c
+++ svm/native.c
@@ -3,12 +3,20 @@
 #include "../memory/MEM.h"
 #include "svm.h"
 
-static SVM_Value native_print(SVM_VirtualMachine* svm, SVM_Value* values,
-                              int arg_count) {
+static SVM_Value native_print(SVM_VirtualMachine* svm, SVM_Value* values,int arg_count) {
     SVM_Value v;
     v.ival = 0;
-    printf("%d\n", values[0].ival);
-    printf("%f\n", values[1].dval);
+    int stack_index = svm->sp - arg_count;
+
+    if (svm->stack_value_type[stack_index] == SVM_INT) {
+        printf("%d\n", values[0].ival);
+    }
+    else if (svm->stack_value_type[stack_index] == SVM_DOUBLE) {
+        printf("%f\n", values[0].dval);
+    }
+    else {
+        printf("print: unkown type\n");
+    }
     return v;
 }
 
@@ -22,6 +30,6 @@ static SVM_Value native_printb(SVM_VirtualMachine* svm, SVM_Value* values,
 }
 
 void add_native_functions(SVM_VirtualMachine* svm) {
-    svm_add_native_function(svm, native_print, "print", 2);
+    svm_add_native_function(svm, native_print, "print", 1);
     svm_add_native_function(svm, native_printb, "printb", 1);
 }
